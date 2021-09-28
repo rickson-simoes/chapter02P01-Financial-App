fccffe6126f00a6e896e4f220b545b25f4c6a4d5
Rickson <rickson.simoes@hotmail.com>
2021-09-27 23:50:33 -0300

    :abc: Google Fonts/ :art: Header

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -3,14 +3,16 @@ import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
   :root {
-    --background: #F0F2F5;
     --red: #e52e4d;
+
     --blue: #5429cc;
     --blue-light: #6933ff;
+
     --text-title: #363f5f;
     --text-body: #969cb3;
+    
     --background:#F0F2F5;
-    --shape: #ffffff;
+    --shape: #FFFFFF;
   }
 
   * {
@@ -31,6 +33,15 @@ export const GlobalStyle = createGlobalStyle`
   body {
     background: var(---background);
     -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 600;
   }
 
   button {
