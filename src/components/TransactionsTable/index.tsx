a61cb65995fbb22eba9efa66cb13b4291aee4ef5
Rickson <rickson.simoes@hotmail.com>
2021-10-05 20:59:19 -0300

    :wrench: Config - Mirage :package:

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,14 +1,24 @@
+import { useEffect } from "react";
 import { Container } from "./styles";
 
 export function TransactionsTable(){
+  useEffect(() => {
+    fetch("http://localhost:3000/api/transactions")
+    .then(response => response.json())
+    .then(data => console.log(data));
+
+  },[]);
+  
   return (
     <Container>
       <table>
         <thead>
-          <th>Título</th>
-          <th>Valor</th>
-          <th>Categoria</th>
-          <th>Data</th>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
         </thead>
 
         <tbody>
