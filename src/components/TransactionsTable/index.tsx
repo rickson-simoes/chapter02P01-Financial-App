1b17ec364b303eb824c5bd29f8b1f7b3c2c039c8
Rickson <rickson.simoes@hotmail.com>
2021-10-05 00:04:31 -0300

    :art: Css - Transactions Table

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,31 @@
+import { Container } from "./styles";
+
+export function TransactionsTable(){
+  return (
+    <Container>
+      <table>
+        <thead>
+          <th>Título</th>
+          <th>Valor</th>
+          <th>Categoria</th>
+          <th>Data</th>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>Desenvolvimento e website</td>
+            <td className="deposit">R$12.000</td>
+            <td>Desenvolvimento</td>
+            <td>20/02/2021</td>
+          </tr>
+          <tr>
+            <td>Aluguel</td>
+            <td className="withdraw">- R$1.100</td>
+            <td>Casa</td>
+            <td>10/02/2021</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  )
+}
