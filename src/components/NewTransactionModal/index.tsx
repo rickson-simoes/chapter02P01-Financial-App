2c732639c54caea5fbff640b6b1a5c7cacbd0ff2
Rickson <rickson.simoes@hotmail.com>
2021-10-11 17:03:00 -0300

    :wrench: :hammer: Estrutura de formulario

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,5 @@
 import Modal from 'react-modal';
+import { Container } from './styles';
 
 Modal.setAppElement("#root");
 
@@ -12,8 +13,24 @@ export function NewTransactionModal({ onRequestClose, isOpen }: ITransactionModa
     <Modal 
       isOpen={ isOpen } 
       onRequestClose={ onRequestClose }
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
     >
-      <h2>Cadastrar transação</h2>
+      <Container>
+        <h2>Cadastrar transação</h2>
+
+        <input placeholder="Título" />
+
+        <input type="number" placeholder="Valor" />
+        
+        <input placeholder="Categoria" />
+
+        <button type="submit">
+          Cadastrar
+        </button>
+      </Container>
+
+
     </Modal>
   );
 }
