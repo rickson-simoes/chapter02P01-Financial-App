392f73c52631f794aa71893e6bab45d6083e1594
Rickson <rickson.simoes@hotmail.com>
2021-10-12 20:29:36 -0300

    :art: Botoes - Entrada/saida

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,7 +1,11 @@
 import Modal from 'react-modal';
+
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+
 import closeImg from '../../assets/close.svg'
 
-import { Container } from './styles';
+import { Container, TransactionTypeContainer } from './styles';
 
 Modal.setAppElement("#root");
 
@@ -32,6 +36,18 @@ export function NewTransactionModal({ onRequestClose, isOpen }: ITransactionModa
         <input placeholder="Título" />
 
         <input type="number" placeholder="Valor" />
+
+        <TransactionTypeContainer>
+          <button type="button">
+            <img src={incomeImg} alt="Entrada" />
+            <span>Entrada</span>
+          </button>
+
+          <button type="button">
+            <img src={outcomeImg} alt="Saída" />
+            <span>Saída</span>
+          </button>
+        </TransactionTypeContainer>
         
         <input placeholder="Categoria" />
 
